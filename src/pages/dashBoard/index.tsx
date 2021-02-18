a7ee1906e8f8b8fb726878e0abb9c292d91e8209
castelvani <ggnei@hotmail.com>
2021-02-18 00:25:04 -0300

    Dashboard initial commit and fixed errors

---
+++ src/pages/dashBoard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logoHandGama from "../../img/logoHandGama.png"
+import {DashBoardPage} from "./style"
+
+const DashBoard: React.FC = () => {
+  return (
+    <DashBoardPage>
+        <img src="logoHandGama" alt=""/>
+    </DashBoardPage>
+  );
+}
+
+export default DashBoard;
