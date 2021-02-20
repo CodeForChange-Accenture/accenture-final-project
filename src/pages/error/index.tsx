1904c79c3b12681523c01b4380ff86be93eeec79
matheusCMendes <matheus.1cruz@hotmail.com>
2021-02-20 15:52:40 -0300

    Css Tela de Erro

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import Logo from "../../img/logo.png";
+import { ErrorStyle } from'./style';
+import { FiArrowLeft, FiAlertTriangle } from 'react-icons/fi';
 
-// import { Container } from './styles';
 
-const error: React.FC = () => {
-  return <div />;
-}
+const Error: React.FC = () => {
+  return (
+    <ErrorStyle>
+      <header>
+      <Link to="/">
+        <img className="logo-gama" src={Logo} alt=""/>
+      </Link>
+      </header>
+    
+      <form>
+        <FiAlertTriangle id="Triangle" color=' #8C52E5' size={120} />
+        <h3 className="title-error">
+            Oops, algo deu errado! Confime seus dados e tente novamente!
+        </h3>
+        <Link to="/">
+            <button type="submit">Voltar<FiArrowLeft size={20}/></button>
+        </Link> 
+      </form>
+    </ErrorStyle>
+  );
+  }
 
-export default error;
+export default Error;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import {Switch, Route} from "react-router-dom";
 import Dashboard from "../pages/dashBoard/index";
 import RecoveryPass from "../pages/recoveryPass/index";
+import Error from "../pages/error/index";
 
 const Routes: React.FC = () => {
     return (
         <Switch>
             <Route path="/dashboard" component={Dashboard}/>
-            <Route path="/" component={RecoveryPass}/>
+            <Route path="/recoveryPass" component={RecoveryPass}/>
+            <Route path="/" component={Error}/>
         </Switch>
     )
 }
