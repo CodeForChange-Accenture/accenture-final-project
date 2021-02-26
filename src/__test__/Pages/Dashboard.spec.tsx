901a658c30d8ad637ff1c268462fb533a6692a28
castelvani <ggnei@hotmail.com>
2021-02-26 18:55:39 -0300

    Unit tests

--- src/__test__/Pages/Dashboard.spec.tsx
+++ src/__test__/Pages/Dashboard.spec.tsx
@@ -1,52 +1,64 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import Dashboard from '../../pages/dashBoard/index';
+import ReactCurrentDispatcher, { render } from "@testing-library/react";
+import React from "react";
+import Dashboard from "../../pages/dashboard/index";
+import * as redux from "react-redux";
+import { AddAccountInfos } from "../../store/modules/user/action";
+import { IBank } from "../../store/modules/user/types";
 
+jest.mock("react-router-dom", () => {
+  return {
+    useHistory: jest.fn(),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
 
+jest.mock("react-redux", () => {
+  return {
+    useDispatch: jest.fn(),
+  };
+});
 
-jest.mock('react-router-dom', () => {
-    return {
-        useHistory: jest.fn(),
-        Link: ({ children }: { children: React.ReactNode }) => children
-    }
-})
+jest.mock("react-redux", () => {
+  return {
+    useSelector: jest.fn(),
+  };
+});
 
-jest.mock('react-redux', () => {
-    return {
-        useSelector: jest.fn(),
-        state: ({ children }: { children: React.ReactNode }) => children
-    }
-})
+jest.mock("react", () => {
+  return {
+    React: jest.fn(),
+    useState: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
 
-jest.mock('react-redux', () => {
-    return {
-        useDispatch: jest.fn(),
-        dispatch: ({ children }: { children: React.ReactNode }) => children
-    }
-})
+jest.mock("react", () => {
+  return {
+    React: jest.fn(),
+    useEffect: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
 
+describe("Home app", () => {
+  it("Find components", () => {
+    const { debug } = render(<Dashboard />);
+    debug();
+  });
+});
 
-jest.mock('react', () => {
-    return {
-        React: jest.fn(),
-        useState: ({ children }: { children: React.ReactNode }) => children
-    }
-})
+describe("dispatch mock", function () {
+  it("should mock dispatch", function () {
+    //arrange
+    const useDispatchSpy = jest.spyOn(redux, "useDispatch");
+    const mockDispatchFn = jest.fn();
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
 
-jest.mock('react', () => {
-    return {
-        React: jest.fn(),
-        useEffect: ({ children }: { children: React.ReactNode }) => children
-    }
-})
+    //action
+    AddAccountInfos(() => Object:IBank);
 
+    //assert
+    expect(mockDispatchFn).toHaveBeenCalledWith(expectedAction);
 
-
-
-
-describe('Home app', () => {
-    it('Find components', () => {
-        const { debug } = render(<Dashboard />)
-        debug();
-    })
-})
+    //teardown
+    useDispatchSpy.mockClear();
+  });
+});
